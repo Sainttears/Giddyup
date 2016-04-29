9ed4d942ee87f0fd03529dce416f0f21f413af7e
Elis <elis.ohrman@gmail.com>
2016-04-29 12:16:46 +0200

    das

--- Unity/SnelHest Prototype/Assets/Scripts/HorseController.cs
+++ Unity/SnelHest Prototype/Assets/Scripts/HorseController.cs
@@ -6,19 +6,20 @@ public class HorseController : MonoBehaviour {
 	PositionChecker pc;
 
 	public Slider staminaBar;
-
-	public float stamina = 100;
-	public float speed = 0;
 	public Vector2 jumpForce;
-	public float baseMove;
-	public float baseMoveMod;
 
+	float stamina = 100;
+	float speed = 0;
+	float baseMoveMod = 20;
 	float slowValue = 1;
 	float crash = 0;
-
 	float speedMod = 0;
 	float speedInp = 1;
-	public float jumpInp = 0;
+	float jumpInp = 0;
+
+	bool grounded;
+	bool resting;
+	bool callOnce;
 
 
 	void Awake(){
@@ -27,54 +28,82 @@ public class HorseController : MonoBehaviour {
 		stamina = 100;
 		speed = 0;
 
+		grounded = true;
 		this.GetComponent<Animator> ().SetBool ("grounded", true);
+
+		resting = false;
+		callOnce = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton (this.name)) {
-			speedInp -= Time.deltaTime;
-		}
-		if (Input.GetButtonUp (this.name)) {
-			speedMod += (speedInp);
-			stamina -= speedInp * 2;
-			speedInp = 1;
-		}
-		if (Input.GetAxis (this.name) < 0) {
-			jumpInp += Time.deltaTime;
-		} else {
-			jumpInp = 0;
-		}
+		this.GetComponent<Animator> ().SetBool ("grounded", grounded);
 
-		if (stamina <= 15)
-			speedMod -= 100 * Time.deltaTime;
-		else
+		if (!resting) {
+			StopCoroutine (waitFor(2));
+
+			if (Input.GetButton (this.name)) {
+				speedInp -= Time.deltaTime;
+			}
+			if (Input.GetButtonUp (this.name)) {
+				speedMod += (speedInp);
+				stamina -= speedInp * 2;
+				speedInp = 1;
+			}
+			if (Input.GetAxis (this.name) < 0) {
+				jumpInp += Time.deltaTime;
+			} else {
+				jumpInp = 0;
+			}
+				
 			speedMod -= 2.5f * Time.deltaTime;
-		stamina += 10 * Time.deltaTime * (1 + pc.GetLead (this.transform) / 10);
+			stamina += 10 * Time.deltaTime * (1 + pc.GetLead (this.transform) / 10);
+
+			speed = (speedMod / 10) * slowValue * baseMoveMod * (stamina / 100) * Time.deltaTime;
+			this.GetComponent<Animator> ().SetFloat ("speed", speed);
+
+			this.transform.position += new Vector3 (speed / 10, 0, 0);
+			if (jumpInp >= 0.4 && stamina >= 10 && grounded) {
+				this.GetComponent<Rigidbody2D> ().AddForce (jumpForce);
+				jumpInp = -10;
+				stamina -= 10;
+				grounded = false;
+			}
+
+			if (stamina <= 10)
+				resting = true;
+
+		} else if (resting) {
+			stamina -= 10 * Time.deltaTime;
+			StartCoroutine (waitFor (3));
+			if (!callOnce) {
+				speedMod = speedMod / 4;
+				callOnce = true;
+			}
+		}
 
 		speedInp = Mathf.Clamp (speedInp, 0, 1);
 		speedMod = Mathf.Clamp (speedMod, 0, 100);
 		stamina = Mathf.Clamp (stamina, 0, 100);
 
-		speed = (speedMod / 10) * slowValue * baseMoveMod * (stamina/100) * Time.deltaTime;
-		this.GetComponent<Animator> ().SetFloat ("speed", speed);
-
-		this.transform.position += new Vector3 (speed / 10, 0, 0);
-		if (jumpInp >= 0.4 && stamina >= 10) {
-			this.GetComponent<Rigidbody2D> ().AddForce (jumpForce);
-			jumpInp = -10;
-			stamina -= 10;
-			this.GetComponent<Animator> ().SetBool ("grounded", false);
-		}
-
-
 		staminaBar.value = stamina;
 		staminaBar.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 1, 0);
 	}
 
+
+	public IEnumerator waitFor (float i){
+		yield return new WaitForSeconds (i);
+		stamina += 50 * Time.deltaTime;
+		if (stamina >= 90) {
+			callOnce = false;
+			resting = false;
+		}
+	}
+
+
 	void OnCollisionEnter2D(Collision2D other){
 		jumpInp = 0;
-		this.GetComponent<Animator> ().SetBool ("grounded", true);
+		grounded = true;
 	}
 	void OnCollisionExit2D(Collision2D other){
 		jumpInp = -10;
