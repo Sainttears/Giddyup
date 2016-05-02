3bd0946d8ddc244ba0391e76c77515b06cfc7eee
Robert <Holytoes@hotmail.se>
2016-05-02 15:27:50 +0200

    PARTICLE MADNESSS

---
+++ Unity/SnelHest Prototype/Assets/Scripts/Cloudmovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloudmovement : MonoBehaviour {
+	public Vector3 targetPosition = new Vector3 (0.26f, 0.35f, 0);
+	public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * speed);
+	
+	}
+}
