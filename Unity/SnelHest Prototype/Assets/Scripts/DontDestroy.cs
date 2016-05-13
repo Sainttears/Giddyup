ce78cb1251bf0138535223b9adef57881700d5fa
Elis <elis.ohrman@gmail.com>
2016-05-13 12:44:12 +0200

    Startscreen functinoality added.

--- Unity/SnelHest Prototype/Assets/Scripts/DontDestroy.cs
+++ Unity/SnelHest Prototype/Assets/Scripts/DontDestroy.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class DontDestroy : MonoBehaviour {
+	public GameObject StartScreen;
+
 	private static DontDestroy _instance;
 	// Use this for initialization
 	void Awake () {
@@ -11,5 +13,16 @@ public class DontDestroy : MonoBehaviour {
 			Destroy (this.gameObject);
 		
 		DontDestroyOnLoad (this.gameObject);
+
+		if (!StartScreen == null) {
+			StartScreen.SetActive (true);
+			Time.timeScale = 0;
+		} else
+			Debug.LogError ("No Startscreen assigned to DontDestroy.cs");
+	}
+
+	public void Begin(){
+		Time.timeScale = 1;
+		StartScreen.SetActive (false);
 	}
 }
